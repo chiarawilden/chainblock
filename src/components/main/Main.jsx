832916bf51cd5bcec02c04bb8a357a8183cfff8c
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-17 16:01:24 +0200

    Creates basic template for main content pages

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,15 @@
 import "./main.css";
+import Home from "../../pages/Home";
 
 export default function Main() {
     return (
         <div className="main">
-            
+            <div className="left">
+                <Home/>
+            </div>
+            <div className="right">
+                
+            </div>
         </div>
     )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import "./page.css";
+
+export default function Home() {
+    return (
+        <div className="page">
+            <h1>Header 1</h1>
+            <h2>Subheader 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi provident 
+            hic repellat qui, quas delectus deserunt autem harum, aut eum eius maxime quaerat 
+            cum voluptatibus ipsum tempora perferendis possimus alias quod impedit rem maiores 
+            nostrum odit. Expedita voluptatum deserunt esse inventore? Iste incidunt alias 
+            totam, sunt ipsum nesciunt modi inventore dignissimos quisquam ut laboriosam 
+            nam consectetur asperiores recusandae in obcaecati molestias sequi earum animi q
+            uod minus doloribus autem dolorum minima. Delectus, iusto eius assumenda 
+            libero ducimus quasi minima eveniet. Minus reprehenderit qui veritatis velit 
+            beatae est maxime maiores tempore, eaque eos. Fugiat, aut quidem ullam 
+            exercitationem iusto eaque sint magnam.</p>
+
+            <h1>Header 2</h1>
+            <h2>Subheader 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi provident 
+            hic repellat qui, quas delectus deserunt autem harum, aut eum eius maxime quaerat 
+            cum voluptatibus ipsum tempora perferendis possimus alias quod impedit rem maiores 
+            nostrum odit. Expedita voluptatum deserunt esse inventore? Iste incidunt alias 
+            totam, sunt ipsum nesciunt modi inventore dignissimos quisquam ut laboriosam 
+            nam consectetur asperiores recusandae in obcaecati molestias sequi earum animi q
+            uod minus doloribus autem dolorum minima. Delectus, iusto eius assumenda 
+            libero ducimus quasi minima eveniet. Minus reprehenderit qui veritatis velit 
+            beatae est maxime maiores tempore, eaque eos. Fugiat, aut quidem ullam 
+            exercitationem iusto eaque sint magnam.</p>
+
+            <h1>Header 3</h1>
+            <h2>Subheader 3</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi provident 
+            hic repellat qui, quas delectus deserunt autem harum, aut eum eius maxime quaerat 
+            cum voluptatibus ipsum tempora perferendis possimus alias quod impedit rem maiores 
+            nostrum odit. Expedita voluptatum deserunt esse inventore? Iste incidunt alias 
+            totam, sunt ipsum nesciunt modi inventore dignissimos quisquam ut laboriosam 
+            nam consectetur asperiores recusandae in obcaecati molestias sequi earum animi q
+            uod minus doloribus autem dolorum minima. Delectus, iusto eius assumenda 
+            libero ducimus quasi minima eveniet. Minus reprehenderit qui veritatis velit 
+            beatae est maxime maiores tempore, eaque eos. Fugiat, aut quidem ullam 
+            exercitationem iusto eaque sint magnam.</p>
+        </div>
+    )
+}
