ac752abf746b8346826cda00d3359e0aebcef620
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-18 00:07:50 +0200

    Component reformat to remove unnecessary divs, adds react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import "./index.css";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/Home.jsx";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Topbar/>
-      <div className="wrapper">
-        <Sidebar/>
-        <Home/>
-      </div>
-    </div>
+    <Router>
+		<Switch>
+			<Route path="/" exact>
+				<Topbar/>
+				<div className="main">
+					<Sidebar/>
+					<Home/>
+				</div>
+			</Route>
+		</Switch>
+    </Router>
   );
 }
 
