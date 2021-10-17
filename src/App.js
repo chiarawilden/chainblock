ede697c595db37d198b88e46f09167b35a7dad43
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-17 13:18:35 +0200

    Completes basic sidebar layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./index.css";
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <div className="app">
       <Topbar/>
+      <div className="wrapper">
+        <Sidebar/>
+        <Main/>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,48 @@
+import "./sidebar.css";
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <aside>
+            <h2>Getting Started</h2>
+                <ul>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                </ul>
+            </aside>
+            <aside>
+            <h2>Section 2</h2>
+                <ul>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                </ul>
+            </aside>
+            <aside>
+            <h2>Section 3</h2>
+                <ul>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                </ul>
+            </aside>
+            <aside>
+            <h2>Section 4</h2>
+                <ul>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                    <li><i class="fas fa-caret-right"></i>This is a line</li>
+                </ul>
+            </aside>
+        </div>
+    );
+};
