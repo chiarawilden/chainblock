e6ea47b0492f8e2ca30afda14602fe5c7fdd70bd
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-23 13:40:41 +0200

    Adds pointer to buttons and adjusts social text

--- src/components/social/Social.jsx
+++ src/components/social/Social.jsx
@@ -5,7 +5,7 @@ export default function Social() {
     return (
         <Link to={{pathname: "https://github.com/chiarawilden/chainblock-docs-app"}} target="_blank">
             <div className="social" >
-                <p style={{margin: "11px 0"}}>Check out our GitHub repo</p>
+                <p style={{margin: "11px 0"}}>Check out on GitHub!</p>
                 <i className="fab fa-github-alt"></i>
             </div>
         </Link>
