e2a0c5d7be4360d255be3af2cbd270e39907c713
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2021-10-23 13:53:13 +0200

    Switches social Link for a href

--- src/components/social/Social.jsx
+++ src/components/social/Social.jsx
@@ -1,13 +1,11 @@
 import "./social.css";
-import {Link} from "react-router-dom";
 
 export default function Social() {
     return (
-        <Link to={{pathname: "https://github.com/chiarawilden/chainblock-docs-app"}} target="_blank">
             <div className="social" >
-                <p style={{margin: "11px 0"}}>Check out on GitHub!</p>
+                <p style={{margin: "11px 0"}}><a href="https://github.com/chiarawilden/chainblock-docs-app">Check out on GitHub!</a></p>
                 <i className="fab fa-github-alt"></i>
             </div>
-        </Link>
+
     )
 }
